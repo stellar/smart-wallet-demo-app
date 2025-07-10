c686b462cbe3cb7ac6ef029e7a368ce93c58038e
Gabriel <gabrielsoarescosta.gsc@gmail.com>
2025-07-10 15:38:31 -0300

    Feat: add SDP Embedded Wallets interface (#17)
    Adds the first integrations with SDP Embedded Wallets API

---
+++ apps/backend/src/interfaces/sdp-embedded-wallets/index.test.ts
@@ -0,0 +1,129 @@
+import axios from 'axios'
+import { getValueFromEnv } from 'config/env-utils'
+import SDPEmbeddedWallets, { CONNECTION_TIMEOUT } from '.'
+import { WalletStatus } from './types'
+
+describe('SDPEmbeddedWallets', () => {
+  const connection = axios.create({
+    baseURL: getValueFromEnv(
+      'SDP_EMBEDDED_WALLETS_URL',
+      'https://stellar-disbursement-platform-backend-dev.stellar.org/embedded-wallets'
+    ),
+    timeout: CONNECTION_TIMEOUT,
+  })
+  const sdpEmbeddedWallets = new SDPEmbeddedWallets(connection)
+
+  afterEach(() => {
+    vi.clearAllMocks()
+    vi.restoreAllMocks()
+  })
+
+  describe('createWallet', () => {
+    test('should create a wallet', async () => {
+      vi.spyOn(connection, 'post').mockResolvedValue({
+        data: {
+          status: 'PENDING',
+        },
+        status: 200,
+      })
+
+      const response = await sdpEmbeddedWallets.createWallet({
+        token: 'token',
+        public_key: 'public_key',
+        credential_id: 'credential_id',
+      })
+      expect(response).toEqual({ status: WalletStatus.PENDING })
+    })
+
+    test('should throw an error if the request fails', async () => {
+      vi.spyOn(connection, 'post').mockRejectedValueOnce(new Error('Request failed'))
+
+      await expect(
+        sdpEmbeddedWallets.createWallet({
+          token: 'token',
+          public_key: 'public_key',
+          credential_id: 'credential_id',
+        })
+      ).rejects.toThrowError()
+    })
+  })
+
+  describe('checkWalletStatus', () => {
+    test('should check the wallet status - PROCESSING case', async () => {
+      vi.spyOn(connection, 'get').mockResolvedValue({
+        data: {
+          status: 'PROCESSING',
+          receiver_contact: 'hello@example.com',
+          contact_type: 'EMAIL',
+        },
+        status: 200,
+      })
+
+      const response = await sdpEmbeddedWallets.checkWalletStatus('token')
+      expect(response).toEqual({
+        status: WalletStatus.PROCESSING,
+        receiver_contact: 'hello@example.com',
+        contact_type: 'EMAIL',
+      })
+    })
+
+    test('should check the wallet status - SUCCESS case', async () => {
+      vi.spyOn(connection, 'get').mockResolvedValue({
+        data: {
+          status: 'SUCCESS',
+          contract_address: 'C...',
+          receiver_contact: 'hello@example.com',
+          contact_type: 'EMAIL',
+        },
+        status: 200,
+      })
+
+      const response = await sdpEmbeddedWallets.checkWalletStatus('token')
+      expect(response).toEqual({
+        status: WalletStatus.SUCCESS,
+        contract_address: 'C...',
+        receiver_contact: 'hello@example.com',
+        contact_type: 'EMAIL',
+      })
+    })
+
+    test('should throw an error if the request fails', async () => {
+      vi.spyOn(connection, 'get').mockRejectedValueOnce(new Error('Request failed'))
+
+      await expect(sdpEmbeddedWallets.checkWalletStatus('token')).rejects.toThrowError()
+    })
+  })
+
+  describe('getContractAddress', () => {
+    test('should get the contract address - PROCESSING case', async () => {
+      vi.spyOn(connection, 'get').mockResolvedValue({
+        data: {
+          status: 'PROCESSING',
+        },
+        status: 200,
+      })
+
+      const response = await sdpEmbeddedWallets.getContractAddress('id')
+      expect(response).toEqual({ status: WalletStatus.PROCESSING })
+    })
+
+    test('should get the contract address - SUCCESS case', async () => {
+      vi.spyOn(connection, 'get').mockResolvedValue({
+        data: {
+          status: 'SUCCESS',
+          contract_address: 'C...',
+        },
+        status: 200,
+      })
+
+      const response = await sdpEmbeddedWallets.getContractAddress('id')
+      expect(response).toEqual({ status: WalletStatus.SUCCESS, contract_address: 'C...' })
+    })
+
+    test('should throw an error if the request fails', async () => {
+      vi.spyOn(connection, 'get').mockRejectedValueOnce(new Error('Request failed'))
+
+      await expect(sdpEmbeddedWallets.getContractAddress('id')).rejects.toThrowError()
+    })
+  })
+})
---
+++ apps/backend/src/interfaces/sdp-embedded-wallets/index.ts
@@ -0,0 +1,78 @@
+import axios, { AxiosInstance } from 'axios'
+import { SingletonBase } from 'api/core/framework/singleton/interface'
+import { AxiosLogger } from 'config/axios-logger'
+import { getValueFromEnv } from 'config/env-utils'
+import { logger } from 'config/logger'
+import {
+  CheckWalletStatusResponse,
+  CreateWalletRequest,
+  CreateWalletResponse,
+  GetContractAddressResponse,
+  SDPEmbeddedWalletsType,
+} from './types'
+
+export const CONNECTION_TIMEOUT = 10000
+
+export default class SDPEmbeddedWallets extends SingletonBase implements SDPEmbeddedWalletsType {
+  private sdpConnection: AxiosInstance
+
+  constructor(connection?: AxiosInstance) {
+    super()
+    this.sdpConnection =
+      connection ??
+      axios.create({
+        baseURL: getValueFromEnv(
+          'SDP_EMBEDDED_WALLETS_URL',
+          'https://stellar-disbursement-platform-backend-dev.stellar.org/embedded-wallets'
+        ),
+        timeout: CONNECTION_TIMEOUT,
+      })
+
+    const axiosLogger = new AxiosLogger(this.constructor.name)
+    this.sdpConnection.interceptors.request.use(axiosLogger.createRequestInterceptor)
+    this.sdpConnection.interceptors.response.use(
+      axiosLogger.createFulfilledResponseInterceptor,
+      axiosLogger.createRejectedResponseInterceptor
+    )
+  }
+
+  public async createWallet(input: CreateWalletRequest): Promise<CreateWalletResponse> {
+    const createWalletUrl = '/'
+
+    try {
+      const requestBody = input
+      const response = await this.sdpConnection.post(createWalletUrl, requestBody)
+
+      return response.data as CreateWalletResponse
+    } catch (error) {
+      logger.error(error, 'SDP - Error creating wallet')
+      throw error
+    }
+  }
+
+  public async checkWalletStatus(token: string): Promise<CheckWalletStatusResponse> {
+    const checkWalletStatusUrl = `/status/${token}`
+
+    try {
+      const response = await this.sdpConnection.get(checkWalletStatusUrl)
+
+      return response.data as CheckWalletStatusResponse
+    } catch (error) {
+      logger.error(error, 'SDP - Error checking wallet status')
+      throw error
+    }
+  }
+
+  public async getContractAddress(id: string): Promise<GetContractAddressResponse> {
+    const getContractAddressUrl = `/${id}`
+
+    try {
+      const response = await this.sdpConnection.get(getContractAddressUrl)
+
+      return response.data as CheckWalletStatusResponse
+    } catch (error) {
+      logger.error(error, 'SDP - Error getting contract address')
+      throw error
+    }
+  }
+}
