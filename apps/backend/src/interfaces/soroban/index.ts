c080756de4f367723b9233b04eeaa355b33c93b5
Phelipe Balisa Folgierini <phelipebalisafolgierini@192.168.1.34>
2025-07-21 11:37:53 -0300

    Soroban interface to interact with contracts

---
+++ apps/backend/src/interfaces/soroban/index.ts
@@ -0,0 +1,90 @@
+import {
+  // Account,
+  // Address,
+  Contract,
+  // FeeBumpTransaction,
+  // hash,
+  Keypair,
+  // Operation,
+  rpc,
+  // StrKey,
+  // Transaction,
+  TransactionBuilder,
+  // xdr,
+} from '@stellar/stellar-sdk'
+import { STELLAR } from 'config/stellar'
+
+import { SimulateContract, SimulationResult } from './types'
+import { ERRORS } from './helpers/errors'
+import { SingletonBase } from 'api/core/framework/singleton/interface'
+
+export class Soroban extends SingletonBase {
+  public rpcClient: rpc.Server
+  public networkPassphrase: string
+  public timeoutInSeconds: number
+  public fee: string
+  public sourceAccountKP: Keypair
+
+  constructor() {
+    super()
+    this.rpcClient = new rpc.Server(STELLAR.SOROBAN_RPC_URL, {
+      allowHttp: STELLAR.SOROBAN_RPC_URL.startsWith('http://'),
+    })
+    this.networkPassphrase = STELLAR.NETWORK_PASSPHRASE
+    this.timeoutInSeconds = 60
+    this.fee = STELLAR.MAX_FEE
+    // this.sourceAccountKP = Keypair.fromSecret(STELLAR.SOURCE_ACCOUNT.PRIVATE_KEY);
+  }
+
+  /**
+   * Simulates a Soroban contract method.
+   * @param contractId - The ID of the Soroban contract.
+   * @param method - The method to call.
+   * @param args - The arguments for the contract call.
+   * @param signers - (Optional) Signers for the contract call.
+   * @returns The transaction and simulation result.
+   */
+  public async simulateContract({
+    contractId,
+    method,
+    args /* , signers */,
+  }: SimulateContract): Promise<SimulationResult> {
+    // Fetch source account
+    const sourceAcc = await this.rpcClient.getAccount(this.sourceAccountKP.publicKey())
+
+    // Initialize the contract
+    const tokenContract = new Contract(contractId)
+    const contractCallOp = tokenContract.call(method, ...args)
+
+    // Build the transaction
+    const tx = new TransactionBuilder(sourceAcc, { fee: this.fee })
+      .addOperation(contractCallOp)
+      .setTimeout(this.timeoutInSeconds)
+      .setNetworkPassphrase(this.networkPassphrase)
+      .build()
+
+    const simulationResponse = await this.rpcClient.simulateTransaction(tx)
+    if (!rpc.Api.isSimulationSuccess(simulationResponse)) {
+      throw new Error(`${ERRORS.TX_SIM_FAILED} (simulation 1): ${simulationResponse}`)
+    }
+
+    // TODO: Sign the entries if signers are provided
+    /* let authEntries: xdr.SorobanAuthorizationEntry[] = simulationResponse.result?.auth ?? [];
+    if (signers && signers.length > 0) {
+      tx = await this.signAuthEntries({
+        authEntries,
+        signers,
+        tx,
+        contractId,
+      });
+
+      // Simulate again after signing
+      simulationResponse = (await this.rpcClient.simulateTransaction(tx)) as rpc.Api.SimulateTransactionSuccessResponse;
+      if (!rpc.Api.isSimulationSuccess(simulationResponse)) {
+        throw new Error(`${ERRORS.TX_SIM_FAILED} (simulation 2): ${simulationResponse}`);
+      }
+    } */
+
+    return { tx, simulationResponse }
+  }
+}
