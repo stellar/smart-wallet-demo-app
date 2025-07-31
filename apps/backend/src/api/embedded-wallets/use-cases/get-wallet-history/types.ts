a680d4a17fc050fc4595152456b301a59b4f69ec
Roberson Costa Dresch <robersoncosta96@gmail.com>
2025-07-31 17:44:44 -0300

    refactor(backend): only return transactions array on wallet history

--- apps/backend/src/api/embedded-wallets/use-cases/get-wallet-history/types.ts
+++ apps/backend/src/api/embedded-wallets/use-cases/get-wallet-history/types.ts
@@ -22,13 +22,6 @@ export const TransactionSchema = z.object({
 
 export type TransactionSchemaT = z.infer<typeof TransactionSchema>
 
-export const ParseSchema = z.object({
-  address: z.string(),
-  transactions: z.array(TransactionSchema),
-})
-
-export type ParseSchemaT = z.infer<typeof ParseSchema>
-
 export const RequestSchema = z.object({
   id: z.string(),
 })
@@ -37,8 +30,7 @@ export type RequestSchemaT = z.infer<typeof RequestSchema>
 
 export const ResponseSchema = createResponseSchema(
   z.object({
-    address: z.string(),
-    transactions: z.array(z.object({})),
+    transactions: z.array(TransactionSchema),
   })
 )
 
