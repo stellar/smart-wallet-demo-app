8546d4868927e187d4f9f13990a541dcc548e018
phelipe-ckl <phelipe.folgierini@ckl.io>
2025-08-27 18:31:07 -0300

    Update apps/backend/src/api/core/entities/ngo/types.ts
    Co-authored-by: Roberson Costa Dresch <robersoncosta96@gmail.com>

--- apps/backend/src/api/core/entities/ngo/types.ts
+++ apps/backend/src/api/core/entities/ngo/types.ts
@@ -7,7 +7,7 @@ export type NgoRepositoryType = {
   getNgoById(vendorId: string): Promise<Ngo | null>
   getNgoByWalletAddress(walletAddress: string): Promise<Ngo | null>
   createNgo(
-    ngo: { name: string; description: string; contractAddress?: string; profileImage?: string },
+    ngo: { name: string; description: string; walletAddress: string; profileImage?: string },
     save?: boolean
   ): Promise<Ngo>
   updateNgo(vendorId: string, data: Partial<Ngo>): Promise<Ngo>
