b94aa4c9bc7db0a2fe4c1eb7f64c334b7a893103
Roberson Costa Dresch <robersoncosta96@gmail.com>
2025-09-10 13:33:05 -0300

    hotfix(web): app title (#183)

--- apps/web/src/app/core/App.tsx
+++ apps/web/src/app/core/App.tsx
@@ -1,17 +1,10 @@
 import { RouterProvider } from '@tanstack/react-router'
-import { useEffect } from 'react'
 
 import ErrorBoundary from './error-boundary'
 import { Providers } from './providers'
 import { router } from './router'
 
 const App = (): JSX.Element => {
-  const envTitle = import.meta.env.VITE_APP_TITLE
-
-  useEffect(() => {
-    document.title = envTitle || 'Smart Wallet'
-  }, [envTitle])
-
   return (
     <ErrorBoundary displayMessage="Ooooppss... An unexpected error occured">
       <Providers>
--- apps/web/src/vite-env.d.ts
+++ apps/web/src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
   readonly VITE_API_URL: string
   readonly VITE_X_API_KEY: string
   readonly VITE_THEME_SWITCH_ENABLED: string
