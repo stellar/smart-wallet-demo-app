9089b800281a2760012868fe3f814ae19993c976
Roberson Costa Dresch <robersoncosta96@gmail.com>
2025-09-12 09:46:09 -0300

    feat(web): add pull refresh at wallet home page (#201)

--- apps/web/src/app/wallet/pages/home/index.tsx
+++ apps/web/src/app/wallet/pages/home/index.tsx
@@ -1,11 +1,15 @@
 import { useNavigate } from '@tanstack/react-router'
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
+import PullToRefresh from 'react-simple-pull-to-refresh'
 
 import { useFeatureFlagsState } from 'src/app/core/helpers'
+import { getFeatureFlags } from 'src/app/core/queries/use-get-feature-flags'
 import logger from 'src/app/core/services/logger'
 import { WalletPagesPath } from 'src/app/wallet/routes/types'
+import { Loading } from 'src/components/atoms'
 import { ImageCard } from 'src/components/organisms'
 import { c } from 'src/interfaces/cms/useContent'
+import { queryClient } from 'src/interfaces/query-client'
 
 import { BannerOptions, FaqOptions, HomeTemplate } from './template'
 import { useDeepLink } from '../../hooks/use-deep-link'
@@ -15,7 +19,7 @@ import { useHandleBehindScenes } from '../../hooks/use-handle-behind-scenes'
 import { useHandleLeftSwags } from '../../hooks/use-handle-left-swags'
 import { useHandleTransferLeftAssets } from '../../hooks/use-handle-transfer-left-assets'
 import { useInitTransfer } from '../../hooks/use-init-transfer'
-import { useGetWallet } from '../../queries/use-get-wallet'
+import { getWallet, useGetWallet } from '../../queries/use-get-wallet'
 import { homeRoute } from '../../routes'
 
 export const Home = () => {
@@ -31,10 +35,10 @@ export const Home = () => {
   ])
 
   // Wallet information
-  const getWallet = useGetWallet({
+  const getWalletQuery = useGetWallet({
     enabled: !loaderDeps.shouldInitTransfer,
   })
-  const walletData = getWallet.data
+  const walletData = getWalletQuery.data
   const isUserAirdropAvailable = walletData ? walletData.is_airdrop_available : false
   const pendingLeftAssets = walletData?.token_balances
     ? walletData.token_balances.every(t => t.balance === 0) && walletData.balance === 0
@@ -52,7 +56,7 @@ export const Home = () => {
 
   // Handle left transfer assets
   const { banner: transferLeftAssetsBanner } = useHandleTransferLeftAssets({
-    enabled: isTransferLeftAssetsActive && !getWallet.isLoading && !pendingLeftAssets,
+    enabled: isTransferLeftAssetsActive && !getWalletQuery.isLoading && !pendingLeftAssets,
   })
 
   const { banner: leftSwagsBanner } = useHandleLeftSwags({
@@ -70,6 +74,10 @@ export const Home = () => {
 
   // Refetch feature flags
   useFeatureFlagsRefetchOnFocus()
+
+  const handleRefresh = useCallback(async () => {
+    await Promise.allSettled([queryClient.forceRefetch(getWallet()), queryClient.forceRefetch(getFeatureFlags())])
+  }, [])
 
   const handleNavbarButtonClick = (item: 'nft' | 'history' | 'profile') => {
     if (item === 'profile') {
@@ -121,14 +129,14 @@ export const Home = () => {
   const isLoadingSwags = useMemo(() => {
     if (!walletData?.swags && loaderDeps.shouldInitTransfer) return true
 
-    return getWallet.isLoading || getWallet.isError
-  }, [getWallet.isError, getWallet.isLoading, loaderDeps.shouldInitTransfer, walletData?.swags])
+    return getWalletQuery.isLoading || getWalletQuery.isError
+  }, [getWalletQuery.isError, getWalletQuery.isLoading, loaderDeps.shouldInitTransfer, walletData?.swags])
 
   const isLoadingBalance = useMemo(() => {
     if (!walletData?.balance && loaderDeps.shouldInitTransfer) return true
 
-    return getWallet.isLoading || getWallet.isError
-  }, [getWallet.isError, getWallet.isLoading, loaderDeps.shouldInitTransfer, walletData?.balance])
+    return getWalletQuery.isLoading || getWalletQuery.isError
+  }, [getWalletQuery.isError, getWalletQuery.isLoading, loaderDeps.shouldInitTransfer, walletData?.balance])
 
   const banners = useMemo(() => {
     const bannersArray: BannerOptions[] = []
@@ -145,18 +153,28 @@ export const Home = () => {
   }, [behindScenesBanner, transferLeftAssetsBanner])
 
   return (
-    <HomeTemplate
-      isLoadingBalance={isLoadingBalance}
-      isLoadingSwags={isLoadingSwags}
-      balanceAmount={walletData?.balance || 0}
-      topBanners={topBanners}
-      banners={banners}
-      products={swags}
-      isProductActionButtonDisabled={isSwagActionButtonDisabled}
-      faq={faq}
-      onNavbarButtonClick={handleNavbarButtonClick}
-      onScanClick={handleScanClick}
-      onProductActionButtonClick={handleSwagClick}
-    />
+    <PullToRefresh
+      onRefresh={handleRefresh}
+      refreshingContent={
+        <div className="flex justify-center items-center mt-5">
+          <Loading color="foregroundPrimary" size="sm" />
+        </div>
+      }
+      pullingContent={<></>}
+    >
+      <HomeTemplate
+        isLoadingBalance={isLoadingBalance}
+        isLoadingSwags={isLoadingSwags}
+        balanceAmount={walletData?.balance || 0}
+        topBanners={topBanners}
+        banners={banners}
+        products={swags}
+        isProductActionButtonDisabled={isSwagActionButtonDisabled}
+        faq={faq}
+        onNavbarButtonClick={handleNavbarButtonClick}
+        onScanClick={handleScanClick}
+        onProductActionButtonClick={handleSwagClick}
+      />
+    </PullToRefresh>
   )
 }
