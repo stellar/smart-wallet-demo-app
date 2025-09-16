8f14b7920725f8b52f19849cc815f138882c2ade
Roberson Costa Dresch <robersoncosta96@gmail.com>
2025-09-16 09:46:39 -0300

    refactor(web): make deeplink live only in memory (#219)

--- apps/web/src/app/core/store/deep-link/index.ts
+++ apps/web/src/app/core/store/deep-link/index.ts
@@ -1,7 +1,4 @@
 import { create } from 'zustand'
-import { createJSONStorage, persist } from 'zustand/middleware'
-
-import { DEEP_LINK_STORAGE_KEY } from 'src/app/auth/constants/storage'
 
 import { DeepLinkStoreFields, DeepLinkStoreState } from './types'
 
@@ -9,16 +6,8 @@ const INITIAL_STATE: DeepLinkStoreFields = {
   deepLink: null,
 }
 
-export const useDeepLinkStore = create<DeepLinkStoreState>()(
-  persist(
-    set => ({
-      ...INITIAL_STATE,
-      setDeepLink: deepLink => set({ deepLink }),
-      clearDeepLink: () => set({ deepLink: null }),
-    }),
-    {
-      name: DEEP_LINK_STORAGE_KEY,
-      storage: createJSONStorage(() => localStorage),
-    }
-  )
-)
+export const useDeepLinkStore = create<DeepLinkStoreState>()(set => ({
+  ...INITIAL_STATE,
+  setDeepLink: deepLink => set({ deepLink }),
+  clearDeepLink: () => set({ deepLink: null }),
+}))
